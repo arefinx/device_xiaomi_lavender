d3eb46b2d981e9055b1029e91c03f7676d25682f
Arefin <ahmedarefinsajjad@gmail.com>
2023-06-11 03:31:34 +0600

    lavender: Proton bringup
    Signed-off-by: Ahmed Arefin <ahmedarefinsajjad@gmail.com>
    
    lavender: Add maintainer overlay
    
    Signed-off-by: Ahmed Arefin <ahmedarefinsajjad@gmail.com>

---
+++ lavender.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# API level, the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Inherit from lavender device
+$(call inherit-product, device/xiaomi/lavender/device.mk)
+
+# Inherit some common Proton stuff
+$(call inherit-product, vendor/proton/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+PROTON_BUILD_VARIANT := OFFICIAL
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_NAME := lavender
+PRODUCT_DEVICE := lavender
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 7
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
